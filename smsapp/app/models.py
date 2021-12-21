533b0386670b929109bf969aed6ba17acb19a86b
Joshua Morris <25271173+jmorris-dev@users.noreply.github.com>
2021-12-21 14:48:53 +0000

    change env vars, now set in heroku

--- smsapp/app/models.py
+++ smsapp/app/models.py
@@ -11,30 +11,19 @@ class UserText(models.Model):
 
     def send_text(self, *args, **kwargs):
 
-        # account_sid = os.environ['TWILIO_ACCOUNT_SID']
-        # auth_token = os.environ['TWILIO_AUTH_TOKEN']
-        # number_from = os.environ['TWILIO_NUMBER']
-        # messaging_service_sid = os.environ['TWILIO_MESSAGING_SERVICE_SID']
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        number_from = os.environ['TWILIO_NUMBER']
+        messaging_service_sid = os.environ['TWILIO_MESSAGING_SERVICE_SID']
 
-        account_sid = 'ACa3eafe7ea1d7040d6b255744e791df60'
-        auth_token = '12e9b3b3daa0501ed2f514bce2a81bd8'
-        messaging_service_sid = 'MG06e87a7afc7e5eed04c7b2ef47160038'
-      
-
+       
         client = Client(account_sid, auth_token)
 
         message = client.messages.create(  
-                              messaging_service_sid='MG06e87a7afc7e5eed04c7b2ef47160038', 
-                              body='sdfsdf',      
+                              messaging_service_sid,
+                              body='test',      
                               to='+17173412994' 
                           ) 
-
-        # message = client.messages.create(
-        #                         messaging_service_sid = 'MG06e87a7afc7e5eed04c7b2ef47160038',
-        #                         body='test',
-        #                         from_='+12345952762',
-        #                         to='+17173412994'
-        #                     )
 
         print(message.sid)
         return super().save(*args, **kwargs)
